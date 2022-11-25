1d0d7007946f2b06a3eaf22fbe92711b27d741ba
ammiraju-viswanath <ammirajuviswanath@gmail.com>
2022-11-25 00:33:34 +0000

    config server integration and internationalisation changes

--- api-gateway-app/src/main/java/com/interview/FallBackController.java
+++ api-gateway-app/src/main/java/com/interview/FallBackController.java
@@ -1,14 +1,34 @@
 package com.interview;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FallBackController {
 
+	@Autowired
+	MessageSource messageSource;
+
+	@Value("${message}")
+	private String message;
+
 	@GetMapping("/fallback")
 	public String message() {
 		return "fallback action";
+	}
+
+	@GetMapping("/properties")
+	public String messageFromProperties() {
+		return messageSource.getMessage("first.message", null, "default message", LocaleContextHolder.getLocale());
+	}
+
+	@GetMapping("/messagefromgit")
+	public String messageValue() {
+		return message;
 	}
 
 }
