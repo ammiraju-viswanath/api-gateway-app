ea0e1e84e4d904dfc3f13a927565c9ea6a0cdb10
ammiraju-viswanath <ammirajuviswanath@gmail.com>
2022-12-02 20:05:55 +0000

    GIT file Changes can now be reflected immediately without a server restart after calling a post method on actuator/refresh.

--- api-gateway-app/src/main/java/com/interview/FallBackController.java
+++ api-gateway-app/src/main/java/com/interview/FallBackController.java
@@ -2,12 +2,14 @@ package com.interview;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class FallBackController {
 
 	@Autowired
